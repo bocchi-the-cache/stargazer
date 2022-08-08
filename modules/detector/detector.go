92793a8b26378a3b0d9daf4ed0ba06dd8c241b30
SPtuan <sptuan@steinslab.io>
2022-08-08 19:09:42 +0800

    feat: add http alert validator

--- modules/detector/detector.go
+++ modules/detector/detector.go
@@ -2,8 +2,11 @@ package detector
 
 import (
 	"github.com/google/uuid"
+	"github.com/sptuan/stargazer/modules/model"
 	"time"
 )
+
+const DETECTOR_HTTP = "http_detector"
 
 type BaseDetector struct {
 	// uuid is unique, mostly for index
@@ -15,10 +18,42 @@ type BaseDetector struct {
 	UpdateTime time.Time
 	Interval   time.Duration
 	Signal     chan struct{}
+	Validators []func(*model.DetectorMessage) error
 }
 
 type Detector interface {
-	Init() error
 	Start() error
 	Stop() error
+}
+
+func (d *BaseDetector) NewMessage() model.DetectorMessage {
+	return model.DetectorMessage{
+		Uuid:       d.Uuid,
+		Name:       d.Name,
+		Type:       d.Type,
+		Target:     d.Target,
+		UpdateTime: time.Now(),
+		Report: model.HealthReport{
+			HealthLevel: model.LevelHealthy,
+			Errs:        []error{},
+			Metric:      map[string]float64{},
+			Logs:        []string{},
+		},
+	}
+}
+
+// RegisterValidator registers a validator function
+// NOTE: Validators only return error of itself.
+// For alert error, put it into message->report->errs.
+func (d *BaseDetector) RegisterValidator(fn func(msg *model.DetectorMessage) error) {
+	d.Validators = append(d.Validators, fn)
+}
+
+func (d *BaseDetector) Validate(msg *model.DetectorMessage) error {
+	for _, fn := range d.Validators {
+		if err := fn(msg); err != nil {
+			return err
+		}
+	}
+	return nil
 }
