aacbe9b6f72c8e2701aa2226c4c3cc2fcb840644
SPtuan <sptuan@steinslab.io>
2022-12-23 18:56:04 +0800

    feat: add data log crud

--- internal/dao/data_log.go
+++ internal/dao/data_log.go
@@ -4,7 +4,6 @@ import (
 	"github.com/sptuan/stargazer/internal/constant"
 	"github.com/sptuan/stargazer/internal/db"
 	"github.com/sptuan/stargazer/internal/entity"
-	"time"
 )
 
 func AddDataLog(dataLog *entity.DataLog) error {
@@ -12,14 +11,20 @@ func AddDataLog(dataLog *entity.DataLog) error {
 	return err
 }
 
-func GetDataLogByTaskIdInTimeRange(taskId int, startTime time.Time, endTime time.Time) ([]entity.DataLog, error) {
+func GetDataLogByTaskIdInTimeRange(taskId int, startTime int, endTime int) ([]entity.DataLog, error) {
 	var dataLogs []entity.DataLog
 	err := db.Db.Where("task_id = ? AND created_at BETWEEN ? AND ?", taskId, startTime, endTime).Find(&dataLogs).Error
 	return dataLogs, err
 }
 
-func GetDataLogByTaskIdLevelInTimeRange(taskId int, level constant.Level, startTime time.Time, endTime time.Time) ([]entity.DataLog, error) {
+func GetDataLogByTaskIdLevelInTimeRange(taskId int, level constant.Level, startTime int, endTime int) ([]entity.DataLog, error) {
 	var dataLogs []entity.DataLog
 	err := db.Db.Where("task_id = ? AND level = ? AND created_at BETWEEN ? AND ?", taskId, level, startTime, endTime).Find(&dataLogs).Error
 	return dataLogs, err
+}
+
+func GetDataLogLastByTaskId(taskId int) (*entity.DataLog, error) {
+	var dataLog entity.DataLog
+	err := db.Db.Where("task_id = ?", taskId).Last(&dataLog).Error
+	return &dataLog, err
 }
